71bf8e40b63713278cfd81126be39de70bfa469a
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-11-08 20:07:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// $("#q1").click(function() {
+//     $("#a1").slideDown("slow");
+//    })
+
+// **********
+
+// $("#q1").click(function() {
+//     $("#a1").slideToggle("slow");
+//     $("#arrow1-down").toggleClass("collapse");
+//     $("#arrow1-up").toggleClass("collapse");
+// })
+
+
+// **********
+
+$(".question").click(function() {
+    $(this).next().fadeToggle("fast");
+    $(this).children().toggleClass("collapse");
+    // $("#a1").slideToggle("slow");
+    // $("#arrow1-down, #arrow1-up").toggleClass("collapse");
+})
+
+// $("#q2").click(function() {
+//     $("#a2").slideToggle("slow");
+//     $("#arrow2-down, #arrow2-up").toggleClass("collapse");
+// })
+
+// $("#q3").click(function() {
+//     $("#a3").slideToggle("slow");
+//     $("#arrow3-down, #arrow3-up").toggleClass("collapse");
+// })
